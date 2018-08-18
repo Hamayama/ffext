7fc7726e894334c5086b6c8f4353ce70dcdb646c
Hamayama <hamay1010@gmail.com>
2018-08-19 00:57:45 +0900

    test chg

--- test.scm
+++ test.scm
@@ -10,18 +10,15 @@
 (use ffext)
 (test-module 'ffext)
 
-(cond-expand
- (gauche.os.windows
-  ;; overwite find-file-in-paths
-  (define (find-file-in-paths name :key paths pred)
-    (apply find-file-in-paths-ext
-           name
-           :ext '("" "exe")
-           (cond-list
-            ((not (undefined? paths)) @ `(:paths ,paths))
-            ((not (undefined? pred))  @ `(:pred  ,pred))
-            ))))
- (else))
+;; overwite find-file-in-paths
+(define (find-file-in-paths name :key paths pred)
+  (apply find-file-in-paths-ext
+         name
+         :ext '("" "exe")
+         (cond-list
+          ((not (undefined? paths)) @ `(:paths ,paths))
+          ((not (undefined? pred))  @ `(:pred  ,pred))
+          )))
 
 (define (testA name expected expr :optional (check test-check))
   (test* name expected expr check)
@@ -30,7 +27,7 @@
 (test-section "find-file-in-paths-ext")
 (testA "1" "<string> or #f" (find-file-in-paths-ext "calc.exe")
        (lambda (e r) (or (string? r) (eq? r #f))))
-(testA "2" "<string> or #f" (find-file-in-paths-ext "notepad" :ext "exe")
+(testA "2" "<string> or #f" (find-file-in-paths-ext "notepad" :ext "exe" :dot ".")
        (lambda (e r) (or (string? r) (eq? r #f))))
 (testA "3" "<string> or #f" (find-file-in-paths-ext "more" :ext '("" "exe" "com"))
        (lambda (e r) (or (string? r) (eq? r #f))))
